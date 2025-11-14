90fe1e18ef9dd877360c64f8cb54d0c3beebf14d
Dao Van Minh <minhokhh@gmail.com>
2025-11-14 22:17:08 +0700

    update util and exception

--- RestaurantBookingApp/service/Repository.java
+++ RestaurantBookingApp/service/Repository.java
@@ -1,4 +1,13 @@
 package service;
 
-public class Repository {
+import util.Persistable;
+import java.util.List;
+import java.util.Optional;
+
+public interface Repository<T, ID> extends Persistable<T> {
+    void add(T entity);
+    void update(T entity);
+    void deleteById(ID id);
+    Optional<T> findById(ID id);
+    List<T> findAll();
 }
